73622b5ea5b7e7570f02e90e09d59da707b07b1b
PouriaBakhshi <39597756+pouriabakhshi@users.noreply.github.com>
2019-01-26 22:08:27 +0330

    Create Multiples of 3 and 5.cpp
    created&solved

---
+++ Multiples of 3 and 5.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+  void multiples(int num){
+int sum = 0;
+for(int x =0; x < num; x++)
+{
+if (x%3 == 0)
+{
+                  sum =sum+x;
+            }
+else if(x%5 == 0)
+{
+sum = sum +x;
+}
+}
+cout <<"The sum is" << sum;
+}
+int main(){
+int number = 1000;
+multiples(number);
+} 
